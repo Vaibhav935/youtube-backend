c7400d7ab32859f0c403eb6c7bff078baa760b5d
Vaibhav935 <vaibhavprajapati935@gmail.com>
2026-01-31 03:53:23 +0530

    user and video model done

--- Steps/Notes.md
+++ Steps/Notes.md
@@ -1,9 +1,16 @@
+<!-- JWT is a Bearer Token -->
+
 1. Professional Backend Setup - Folder structure setup
 2. Connected Database 
 3. Server Setup
-4. Implemented necessary middlewares
+4. Implemented necessary middlewares (like for url encoding and json)
 5. Understanding utils 
     - asyncHandler - for handling all async task of backend
     - ApiError - for setting a standard to send error 
     - ApiResponse - for setting a standard to send response
-6. 
+6. Made User and Video models.
+7. Installed mongooseAggregatePaginate.
+8. MySchema.pre() (hook)(middleware) implementing.
+    - .pre - to run code just before saving data in db
+9. Checking pass in UserSchema only
+10. Written methods to generate Access and Refresh token in userSchema.
---
+++ src/models/video.model.js
@@ -0,0 +1,46 @@
+import { mongoose, Schema, model } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchema = new Schema(
+  {
+    videoFile: {
+      type: String, // cloudinary url
+      required: true,
+    },
+    thumbnail: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    duration: {
+      type: Number, // cloudinary url
+      default: 0,
+    },
+    views: {
+      type: Number,
+      default: 0,
+    },
+    isPublished: {
+      type: Boolean,
+      default: true,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "UserModel",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+videoSchema.plugin(mongooseAggregatePaginate)
+
+export const VideoModel = model("Video", videoSchema);
